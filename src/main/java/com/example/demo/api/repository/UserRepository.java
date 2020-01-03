590349c637c48dff74812cbe2b0e456ffdf8ee93
Arthur <arthur.dourado@memora.com.br>
2020-01-03 17:56:39 -0300

    Criando o serviço de ticket

--- src/main/java/com/example/demo/api/repository/UserRepository.java
+++ src/main/java/com/example/demo/api/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.api.entity.User;
@@ -7,5 +9,9 @@ import com.example.demo.api.entity.User;
 public interface UserRepository extends MongoRepository<User, String>{
 	
 	User findByEmail(String email);
+	
+	Optional<User> findById(String id);
+	
+	void deleteById(String id);
 
 }
